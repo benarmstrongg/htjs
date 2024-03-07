7a8bd5288d5c69e0b8a0a8e74254a80c542f140b
ben <benarmstrongg@gmail.com>
2024-03-06 22:23:08 -0500

    Clean up dom tests.

--- test/dom.test.ts
+++ test/dom.test.ts
@@ -7,7 +7,16 @@ const { TextEncoder, TextDecoder } = require('util');
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
 
-import { JSDOM } from 'jsdom';
+// @ts-ignore setup react
+global.IS_REACT_ACT_ENVIRONMENT = true;
+
+import PreactTestUtils from 'preact/test-utils';
+import ReactTestUtils from 'react-dom/test-utils';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import * as preact from 'preact';
+import { $, _, bind } from '../src';
+
 const PAGE_HTML = `
 <!DOCTYPE html>
 <html>
@@ -19,48 +28,43 @@ const PAGE_HTML = `
     </body>
 </html>
 `;
-global.window.document.write(PAGE_HTML);
-const { window } = new JSDOM(PAGE_HTML);
-const { document } = window;
-
-import PreactTestUtils from 'preact/test-utils';
-import ReactTestUtils from 'react-dom/test-utils';
-// @ts-ignore
-global.IS_REACT_ACT_ENVIRONMENT = true;
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import * as preact from 'preact';
-import { $, _, bind } from '../src';
 
 describe('DOM Render', () => {
-    const App = () =>
+    beforeEach(() => {
+        document.write(PAGE_HTML);
+    });
+
+    const App = $((props: { lib: 'preact' | 'react' }) =>
         _.div({ id: 'app' })(
-            _.p('hello'),
+            _.p(`hello from ${props.lib}`),
             _.a({ href: 'https://test.com' })('test')
-        );
+        )
+    );
 
     test('ReactDOM.createRoot.render', () => {
         bind(React.createElement);
         ReactTestUtils.act(() => {
             ReactDOM.createRoot(document.getElementById('root')!).render(
-                $(React.StrictMode)(App())
+                $(React.StrictMode)(App({ lib: 'react' })())
             );
         });
         const root = document.getElementById('root')!;
         expect(root.innerHTML).toBe(
-            '<div id="app"><p>hello</p><a href="https://test.com">test</a></div>'
+            '<div id="app"><p>hello from react</p><a href="https://test.com">test</a></div>'
         );
     });
 
     test('preact.render', () => {
         bind(preact.h);
         PreactTestUtils.act(() => {
-            preact.render(App(), global.document.getElementById('root')!);
+            preact.render(
+                App({ lib: 'preact' })(),
+                document.getElementById('root')!
+            );
         });
-        const root = global.document.getElementById('root')!;
+        const root = document.getElementById('root')!;
         expect(root.innerHTML).toBe(
-            '<div id="app"><p>hello</p><a href="https://test.com">test</a></div>'
+            '<div id="app"><p>hello from preact</p><a href="https://test.com">test</a></div>'
         );
     });
 });
